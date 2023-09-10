6c5fd8cda7a4cfbfc9083d161bc23840ec2b02c0
Javier Ubillos <javier@ubillos.org>
2023-09-10 20:59:32 +0200

    Flexbox + init critics.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import Reviewer from './components/Reviewer';
+
 type FullFeedback = {
   [key: string]: Feedback
 }
@@ -10,10 +12,22 @@ type Feedback = {
   "feedback": string
 }
 
+const initReviewers = () => {
+  return {
+    "the critic": {
+      score: 0,
+      feedback: "You haven't even started yet!"
+    },
+    "the grammarian": {
+      score: 0,
+      feedback: "You haven't even started yet!"
+    },
+  }
+}
+
 const App = () => {
   const [postContent, setPostContent] = React.useState<string>("No post content yet")
-  const [fullFeedback, setFullFeedback] = React.useState<FullFeedback>(
-    {})
+  const [fullFeedback, setFullFeedback] = React.useState<FullFeedback>(() => initReviewers())
 
 
   const getFeedback = (postContent: string) => {
@@ -36,16 +50,25 @@ const App = () => {
   }
 
   return (
-    <div className="App" style={{ flex: 2 }}>
+    <div className="App" style={{ display: "flex", flexDirection: "column" }}>
       <button name="getFeedback" onClick={() => getFeedback(postContent)}>Get Feedback</button>
-      <div style={{ flex: 1 }}>
-        <textarea id="postContent"
-          cols={50} rows={50}
-          value={postContent}
-          onChange={(e) => setPostContent(e.currentTarget.value)} ></textarea>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div>
+          <textarea id="postContent"
+            cols={50} rows={50}
+            value={postContent}
+            onChange={(e) => setPostContent(e.currentTarget.value)} ></textarea>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          {Object.keys(fullFeedback).map((reviewerName) => (
+            <Reviewer name={reviewerName}
+              score={fullFeedback[reviewerName].score}
+              feedback={fullFeedback[reviewerName].feedback} />
+          ))}
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
 
