893a62dc8839cca0a4a3aba43c5d97fc6a740f47
Javier Ubillos <javier@ubillos.org>
2023-09-12 21:44:05 +0200

    Add list of reviewers with slug => name translation.

--- src/App.tsx
+++ src/App.tsx
@@ -17,18 +17,36 @@ const initReviewers = () => {
   return {
     "the critic": {
       score: 0,
-      feedback: "You haven't even started yet!"
+      feedback: "You haven't even started yet!",
+      enabled: true
     },
     "the grammarian": {
       score: 0,
-      feedback: "You haven't even started yet!"
+      feedback: "You haven't even started yet!",
+      enabled: true
     },
+  }
+}
+
+const reviewerNames: { [key: string]: { name: string } } = {
+  "storyTeller": {
+    name: "The story teller"
+  },
+  "teacher": {
+    name: "The teacher"
+  },
+  "hemingway": {
+    name: "Hemmingway"
+  },
+  "scientist": {
+    name: "The scientist"
   }
 }
 
 const App = () => {
   const [postContent, setPostContent] = React.useState<string>("No post content yet")
   const [fullFeedback, setFullFeedback] = React.useState<FullFeedback>(() => initReviewers())
+  const [muteSet, setMuteSet] = React.useState<Set<string>>(new Set())
 
   const getFeedback = (postContent: string) => {
     const requestOptions = {
@@ -59,13 +77,19 @@ const App = () => {
       <div className="ReviewersPanel">
         <button name="getFeedback" onClick={() => getFeedback(postContent)}>Get Feedback</button>
 
-        {Object.keys(fullFeedback).map((reviewerName) => (
+        {Object.keys(fullFeedback).map((reviewerSlug) => (
           <div>
-            <Reviewer name={reviewerName}
-              enabled={false}
-              key={reviewerName}
-              score={fullFeedback[reviewerName].score}
-              feedback={fullFeedback[reviewerName].feedback} />
+            <button name="toggleReviewer" onClick={() => {
+              muteSet.has(reviewerSlug) ? muteSet.delete(reviewerSlug) : muteSet.add(reviewerSlug)
+              setMuteSet(new Set(muteSet))
+            }}>
+              {muteSet.has(reviewerSlug) ? "Unmute" : "Mute"}
+            </button>
+            <Reviewer name={reviewerNames[reviewerSlug]?.name || "Undefined name"}
+              enabled={!muteSet.has(reviewerSlug)}
+              key={reviewerSlug}
+              score={fullFeedback[reviewerSlug].score}
+              feedback={fullFeedback[reviewerSlug].feedback} />
           </div>
         ))}
       </div>
