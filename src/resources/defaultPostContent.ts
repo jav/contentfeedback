c9ffdb27fe94436b29710e756c6caed2abaa5574
Javier Ubillos <javier@ubillos.org>
2023-09-17 18:00:55 +0200

    Add defaultPostContent.ts

---
+++ src/resources/defaultPostContent.ts
@@ -0,0 +1,42 @@
+const defaultPostContent = `
+# Content feedback
+## Feedback by (GPT powered) experts!
+
+
+I've wanted to share my thoughts, ideas and descriptions of what I encounter
+in my professional life.
+
+I long to be one of those who share useful blog posts!
+
+But, I struggle. I tend to write dry and formally. I often write in a formal
+'deduction' mode.
+
+My texts could look like:
+> A, My thinking tends to be oriented towards axioms and dependencies
+> 
+> B. When I want to share a thought, it is just that, I'm sharing how I think
+>
+> ∴ My writing tends to look like  propositional logic
+ 
+
+## Tell a story!
+### Great ideas are best transmitted as stories!
+I've been told, over and over again, that I should introduce ideas as stories.
+"You need to tell a story".
+
+I struggle with the concept. I searched on-line for concrete tips, but all I could
+find where concepts such as "tell a story about a person2 "use cliff-hangers", 
+"take the reader on a journey".
+
+## Find a place to get help
+I listened to a talk about using ChatGPT for programming.
+I got _very_ inspired, I could ask ChatGPT for coaching, from different perspectives.
+
+This is what drove me to create this site!
+
+I can now get feedback on what I write, and concrete suggestions on how to improve 
+the writing from several different perspectives!
+
+`
+
+export default defaultPostContent
